f731c3915da0ffa059383d4c8d3cdfc6162da74c
nikijey031-oss <nikijey031@gmail.com>
2026-08-27 23:23:46 +0530

    Add event association to participants

--- src/main/java/model/Participant.java
+++ src/main/java/model/Participant.java
@@ -4,17 +4,42 @@ public class Participant {
 
     private int participantId;
     private String name;
+    private int eventId;
+
+    public Participant() {
+    }
 
     public Participant(int participantId, String name) {
+        this(participantId, name, 0);
+    }
+
+    public Participant(int participantId, String name, int eventId) {
         this.participantId = participantId;
         this.name = name;
+        this.eventId = eventId;
     }
 
     public int getParticipantId() {
         return participantId;
     }
 
+    public void setParticipantId(int participantId) {
+        this.participantId = participantId;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
     }
 }
